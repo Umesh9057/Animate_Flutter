664e2ff086a6d06b8bc17391dc8e334d17a7cea4
Umesh <umekum9057@gmail.com>
2025-12-11 23:06:14 +0530

    Update home_page_view.dart

--- lib/src/modules/home_page_view.dart
+++ lib/src/modules/home_page_view.dart
@@ -9,6 +9,8 @@ class HomePageView extends StatefulWidget {
 }
 
 class _HomePageViewState extends State<HomePageView> {
+
+  var arrIndex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
   
   @override
   Widget build(BuildContext context) {
@@ -19,8 +21,28 @@ class _HomePageViewState extends State<HomePageView> {
         centerTitle: true,
       ),
 
-      body: Text('Hello'),
+      body:ListWheelScrollView(
+        itemExtent: 200,
+        children: arrIndex
+            .map(
+              (value) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(11),
+                    color: Colors.cyan.shade200,
+                  ),
+                  width: double.infinity,
+                  child: Center(
+                    child: Text('$value', style: TextStyle(fontSize: 21)),
+                  ),
+                ),
+              ),
+            )
+            .toList(),
+      ),
     );
   }
 }
+
 
