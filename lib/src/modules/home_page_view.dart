7e375ded87eda4174e244d0fa6b946d282805dde
Umesh <umekum9057@gmail.com>
2025-12-11 20:55:50 +0530

    Update home_page_view.dart

--- lib/src/modules/home_page_view.dart
+++ lib/src/modules/home_page_view.dart
@@ -9,6 +9,27 @@ class HomePageView extends StatefulWidget {
 }
 
 class _HomePageViewState extends State<HomePageView> {
+
+   bool isfirst = true;
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   Timer(Duration(seconds: 4), () {
+  //     reload();
+  //   });
+  // }
+
+  void reload() {
+    setState(() {
+      if (isfirst) {
+        isfirst = false;
+      } else {
+        isfirst = true;
+      }
+    });
+  }
+
   
   @override
   Widget build(BuildContext context) {
@@ -19,8 +40,42 @@ class _HomePageViewState extends State<HomePageView> {
         centerTitle: true,
       ),
 
-      body: Text('Hello'),
+      body:  Center(
+        child: Column(
+          children: [
+            AnimatedCrossFade(
+              firstChild: Image.asset(
+                'assets/images/Itachi.webp',
+                width: 500,
+                height: 500,
+              ),
+              secondChild: Image.asset(
+                'assets/images/Boruto.png',
+                width: 400,
+                height: 400,
+              ),
+              crossFadeState: isfirst
+                  ? CrossFadeState.showFirst
+                  : CrossFadeState.showSecond,
+              duration: Duration(seconds: 4),
+              sizeCurve: Curves.fastOutSlowIn,
+            ),
+            SizedBox(height: 11),
+            ElevatedButton(
+              onPressed: () {
+                reload();
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.cyan,
+                foregroundColor: Colors.black,
+              ),
+              child: Text('Change Image'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
 
