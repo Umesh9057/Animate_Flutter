ef89ca72adea5c3b48e7fc3faa4bd1c4e6f004f3
Umesh <umekum9057@gmail.com>
2025-12-11 13:32:01 +0530

    Update home_page_view.dart

--- lib/src/modules/home_page_view.dart
+++ lib/src/modules/home_page_view.dart
@@ -9,15 +9,7 @@ class HomePageView extends StatefulWidget {
 }
 
 class _HomePageViewState extends State<HomePageView> {
-  var _width = 200.0;
-  var _height = 100.0;
-  bool flag = true;
-
-  Decoration myDecor = BoxDecoration(
-    borderRadius: BorderRadius.circular(3),
-    color: Colors.blueAccent,
-  );
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,51 +19,8 @@ class _HomePageViewState extends State<HomePageView> {
         centerTitle: true,
       ),
 
-      body: Center(
-        child: Column(
-          children: [
-            AnimatedContainer(
-              duration: Duration(seconds: 2),
-              width: _width,
-              height: _height,
-              curve: Curves.bounceOut,
-              decoration: myDecor,
-            ),
-            SizedBox(height: 11),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  if (flag) {
-                    _width = 120.0;
-                    _height = 150.0;
-                    flag = false;
-                    myDecor = BoxDecoration(
-                      borderRadius: BorderRadius.circular(21),
-                      color: Colors.greenAccent,
-                    );
-                  } else {
-                    _height = 100.0;
-                    _width = 200.0;
-                    flag = true;
-                    myDecor = BoxDecoration(
-                      borderRadius: BorderRadius.circular(3),
-                      color: Colors.blueAccent,
-                    );
-                  }
-                });
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.cyan,
-                foregroundColor: Colors.black,
-              ),
-              child: Text(
-                'Animate',
-                style: TextStyle(fontSize: 21, fontWeight: FontWeight.w200),
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: Text('Hello'),
     );
   }
 }
+
